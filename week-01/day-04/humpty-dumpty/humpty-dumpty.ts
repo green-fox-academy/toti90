d2c0abf3c111a51daae1de36109953a766629ea1
Tamas Toth <tothtomi7@gmail.com>
2019-04-04 12:16:51 +0200

    add humpty-dumpty solution

---
+++ week-01/day-04/humpty-dumpty/humpty-dumpty.ts
@@ -0,0 +1,8 @@
+'use strict';
+
+// Modify this program to console.log Humpty Dumpty riddle correctly
+
+console.log('All the king\'s horses and all the king\'s men');
+console.log('Couldn\'t put me back together again');
+console.log('All the king\'s horses and all the king\'s men');
+console.log('Couldn\'t put me back together again');
