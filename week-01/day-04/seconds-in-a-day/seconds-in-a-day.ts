7add90c0e1cf12b606b94ae0915899a32ad73b0e
Tamas Toth <tothtomi7@gmail.com>
2019-04-04 15:05:37 +0200

    Addd seconds-in-a-day solution

---
+++ week-01/day-04/seconds-in-a-day/seconds-in-a-day.ts
@@ -0,0 +1,12 @@
+'use strict';
+
+let currentHours: number = 14;
+let currentMinutes: number = 34;
+let currentSeconds: number = 42;
+
+// Write a program that prints the remaining seconds (as an integer) from a
+// day if the current time is represented by these variables
+
+let oneDayInSeconds = 24*60*60;
+let passdSeconds = currentHours*60*60+currentMinutes*60+currentSeconds;
+console.log(oneDayInSeconds-passdSeconds);
