84a9073e3a6202e01a84811da75cd5472a7b3f86
Tamas Toth <tothtomi7@gmail.com>
2019-04-04 13:07:35 +0200

    add new version of coding-hours

--- week-01/day-04/coding-hours/coding-hours.ts
+++ week-01/day-04/coding-hours/coding-hours.ts
@@ -5,8 +5,11 @@
 //
 // Print how many hours is spent with coding in a semester by an attendee,
 // if the attendee only codes on workdays.
-console.log("If attendee codes only workdays, working hours: " + 17*5*6 + " h");
+let allHoursSemester = 17*5*6;
+console.log("If attendee codes only workdays, working hours: " + allHoursSemester + " h");
 
 // Print the percentage of the coding hours in the semester if the average
 // work hours weekly is 52
-console.log("The coding hours if average work hours weekly is 52 h: " + 52*17 + " h");
+let codeHoursAWeek = 5*6;
+let workHoursAWeek = 52;
+console.log("The percentage of coding hours if the working hours is 52 h " + ((codeHoursAWeek)/workHoursAWeek)*100 + " %");
