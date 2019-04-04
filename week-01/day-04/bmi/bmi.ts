2a48b4d8db234aa199f5c0dd48fe07901f28a4cf
Tamas Toth <tothtomi7@gmail.com>
2019-04-04 13:23:58 +0200

    add bmi solution

---
+++ week-01/day-04/bmi/bmi.ts
@@ -0,0 +1,8 @@
+'use strict';
+
+let massInKg: number = 81.2;
+let heightInM: number = 1.78;
+
+// Print the Body mass index (BMI) based on these values
+let bmiIndex = massInKg/(heightInM**2);
+console.log("Your BMI index is " + bmiIndex);
