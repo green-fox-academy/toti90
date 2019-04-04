3887a4958f9791bea6574094331b78fc090bf1d0
Tamas Toth <tothtomi7@gmail.com>
2019-04-04 13:31:46 +0200

    add define-basic-info solution

---
+++ week-01/day-04/define-basic-info/define-basic-info.ts
@@ -0,0 +1,18 @@
+'use strict';
+
+// Define several things as a variable, then print their values
+// Your name as a string
+// Your age as a number
+// Your height in meters as a number
+// Whether you are married or not as a boolean
+let myName: string = "Tomi";
+let myAge: number = 29;
+let myHeight: number = 1.82;
+let amIMarriaged: boolean = false;
+
+console.log("My name is: " + myName);
+console.log("My age is: " + myAge);
+console.log("My height is: " + myHeight);
+console.log("Am I marriaged?: " + amIMarriaged);
+
+
