e9701d4eca01cf248eb92b91013a501173cce3ba
Tamas Toth <tothtomi7@gmail.com>
2019-04-04 16:40:41 +0200

    Add draw-pyramid solution

---
+++ week-01/day-04/draw-pyramid/draw-pyramid.ts
@@ -0,0 +1,17 @@
+'use strict';
+
+let lineCount: number = 4;
+
+// Write a program that draws a
+// pyramid like this:
+//
+//    *
+//   ***
+//  *****
+// *******
+//
+// The pyramid should have as many lines as lineCount is
+let result : string = "something";
+for (let i = 1; i <= lineCount; i++) {
+    console.log(" ".repeat(lineCount-i) + "*".repeat(i) + "*".repeat(i-1));
+}
