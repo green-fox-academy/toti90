4592676b1325bfd1ea020cee4d30de41bed10a04
Tamas Toth <tothtomi7@gmail.com>
2019-04-04 12:40:26 +0200

    add some exercises (coding-hours, hello-others, introduce-yourself, two-numbers)

---
+++ week-01/day-04/hello-others/hello-others.ts
@@ -0,0 +1,10 @@
+'use strict';
+// Greet 3 of your classmates with this program, in three separate lines like:
+//
+// Hello, Esther!
+// Hello, Mary!
+// Hello, Joe!
+
+console.log("Hello Soma!");
+console.log("Hello Jani!");
+console.log("Hello Nóri!");
