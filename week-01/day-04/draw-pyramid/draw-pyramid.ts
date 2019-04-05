5386278083fa18cd404387d644acdd11325fc85c
Tamas Toth <tothtomi7@gmail.com>
2019-04-05 10:46:13 +0200

    refactor (draw-pyramid): delete one unnecessary line

--- week-01/day-04/draw-pyramid/draw-pyramid.ts
+++ week-01/day-04/draw-pyramid/draw-pyramid.ts
@@ -1,6 +1,6 @@
 'use strict';
 
-let lineCount: number = 50;
+let lineCount: number = 6;
 
 // Write a program that draws a
 // pyramid like this:
@@ -11,7 +11,6 @@ let lineCount: number = 50;
 // *******
 //
 // The pyramid should have as many lines as lineCount is
-let result : string = "something";
 for (let i = 1; i <= lineCount; i++) {
     console.log(" ".repeat(lineCount-i) + "*".repeat(i) + "*".repeat(i-1));
 }
