f32a3bcd1d10214790f5706d096482d21d33675f
Tamas Toth <tothtomi7@gmail.com>
2019-04-05 10:53:44 +0200

    feat (substr): write a sentence if substr not in original string insted of -1

--- week-01/day-04/substr/substr.ts
+++ week-01/day-04/substr/substr.ts
@@ -2,8 +2,13 @@
 //  Create a function that takes two strings as a parameter
 //  Returns the starting index where the second one is starting in the first one
 //  Returns `-1` if the second string is not in the first one
-function substr(str: string, keyword: string) : number {
-    return str.indexOf(keyword);
+function substr(str: string, keyword: string) {
+    let indexofNumber = str.indexOf(keyword);
+    if (indexofNumber !== -1) {
+        return indexofNumber;
+    }else {
+        return "Your searched word couldn't be found in the original sentence";
+    };
 }
 //  Example
 // should print: `17`
