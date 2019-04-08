5db917418a57adfbacbedc42438d5440d7e86262
Tamas Toth <tothtomi7@gmail.com>
2019-04-08 14:02:25 +0200

    feat(double-items) add double-items solution

---
+++ week-02/day-01/double-items/double-items.ts
@@ -0,0 +1,12 @@
+`use strict`;
+export{};
+
+// -  Create an array variable named `drinks` with the following content: `['Gin', 'Whiskey', 'Wine', 'Beer']`
+// -  Double all the strings in the array, use a built in array method instead of a loop
+// It should print: ['GinGin', 'WhiskeyWhiskey', 'WineWine', 'BeerBeer']`
+
+let drinks: string[] = ['Gin', 'Whiskey', 'Wine', 'Beer'];
+
+drinks = drinks.map(x => x+x);
+
+console.log(drinks);
