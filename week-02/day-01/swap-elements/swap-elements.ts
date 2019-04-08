5995ed1ed72e1165fa28e0f0f8c7bbb907ad0db1
Tamas Toth <tothtomi7@gmail.com>
2019-04-08 20:36:31 +0200

    feat(swap-elements): swap the 1st and 3rd element

--- week-02/day-01/swap-elements/swap-elements.ts
+++ week-02/day-01/swap-elements/swap-elements.ts
@@ -6,7 +6,10 @@
 let abc: String[] = ["Arthur", "Boe", "Chloe"];
 
 let firstElement = abc[0];
+let thirdElement = abc[2];
+abc.pop();
 abc.shift();
 abc.splice(1, 0, firstElement);
+abc.splice(0, 0, thirdElement);
 console.log(abc);
 
