8c8de01098e3aecf616cc7e80145149d874d80d7
tothtomi <tothtomi7@gmail.com>
2019-04-09 14:45:00 +0200

    feat(shopping-list): add solution

---
+++ week-02/day-02/shopping-list/shopping-list.ts
@@ -0,0 +1,19 @@
+`use strict`;
+export{};
+
+// We are going to represent a shopping list in a list containing strings.
+
+// Create a list with the following items.
+// Eggs, milk, fish, apples, bread and chicken
+// Create an application which solves the following problems.
+// Do we have milk on the list?
+// Do we have bananas on the list?
+
+let shoppingList = ["eggs", "milk", "fish", "apples", "bread", "chicken"];
+
+function shop(item:any, list: string[]){
+    return shoppingList.indexOf(item) !== -1 ? `${item} is in the list`: `no ${item} in the list`
+}
+
+console.log(shop("eggs", shoppingList));
+console.log(shop("bananas", shoppingList));
