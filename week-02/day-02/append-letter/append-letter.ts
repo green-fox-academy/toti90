b483ebde2623c9f7025e075f24c71f4d02ea7a2b
tothtomi <tothtomi7@gmail.com>
2019-04-09 16:49:13 +0200

    feat(append-a): add solution

---
+++ week-02/day-02/append-letter/append-letter.ts
@@ -0,0 +1,15 @@
+'use strict';
+// Create a function called "appendA()" that adds "a" to every string in the far list.
+// The parameter should be a list.
+
+let far: string[] = ['bo', 'anacond', 'koal', 'pand', 'zebr'];
+
+function appendA(list:any[]) {
+    return list.map(x => x + "a")
+}
+
+console.log(appendA(far));
+
+// The output should be: 'boa', 'anaconda', 'koala', 'panda', 'zebra'
+
+export = appendA;
