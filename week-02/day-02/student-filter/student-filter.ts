99e8546c775ec4ce3e4c2830406df3c8c1c57309
tothtomi <tothtomi7@gmail.com>
2019-04-10 16:39:33 +0200

    feat(student-filter): add solution

--- week-02/day-02/student-filter/student-filter.ts
+++ week-02/day-02/student-filter/student-filter.ts
@@ -13,8 +13,12 @@ const students: any[] = [
 //  - how many candies they have on average
 
 function studentFunc2(list:any[]) {
-    list.filter(x => x["candies"]>2)
-    list.map(x => )
+    let members:string[] = [];
+    let counts: number = 0
+    list = list.filter(x => x["candies"]>2)
+    list.map(x => members.push(x["name"]))
+    list.map(x =>  counts += x["candies"])
+    return `they has got more than 4 candies: ${members}\nand average they have got ${counts/members.length} candies`
 }
 
 console.log(studentFunc2(students));
