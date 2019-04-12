5079d75c9eb8c2050b4bd4c2e02b101ed92b9bfe
tothtomi <tothtomi7@gmail.com>
2019-04-12 15:32:19 +0200

    refact(circle,tree,triangle): add semicolumns

--- week-02/day-05/circle/circle.ts
+++ week-02/day-05/circle/circle.ts
@@ -11,13 +11,13 @@ function circle(x:number,y:number,size:number):void {
   }
   ctx.beginPath();
   ctx.strokeStyle = `rgb(${Math.random() * 255},${Math.random() * 255},${Math.random() * 255})`
-  ctx.arc(x,y,size,0,Math.PI*2)
-  ctx.stroke()
-  const position_1 = {x:x,y:y-size/2}
-  const position_2 = {x:x-Math.cos(Math.PI/6)*size/2,y:y+Math.sin(Math.PI/6)*size/2}
-  const position_3 = {x:x+Math.cos(Math.PI/6)*size/2,y:y+Math.sin(Math.PI/6)*size/2}
-  circle(position_1.x,position_1.y,size/2)
-  circle(position_2.x,position_2.y,size/2)
-  circle(position_3.x,position_3.y,size/2)
+  ctx.arc(x,y,size,0,Math.PI*2);
+  ctx.stroke();
+  const position_1 = {x:x,y:y-size/2};
+  const position_2 = {x:x-Math.cos(Math.PI/6)*size/2,y:y+Math.sin(Math.PI/6)*size/2};
+  const position_3 = {x:x+Math.cos(Math.PI/6)*size/2,y:y+Math.sin(Math.PI/6)*size/2};
+  circle(position_1.x,position_1.y,size/2);
+  circle(position_2.x,position_2.y,size/2);
+  circle(position_3.x,position_3.y,size/2);
 }
 circle(300,300,300)
--- week-02/day-05/tree/tree.ts
+++ week-02/day-05/tree/tree.ts
@@ -4,23 +4,29 @@ const canvas = document.querySelector('.main-canvas') as HTMLCanvasElement;
 const ctx = canvas.getContext('2d');
 
 
-function tree(size) {
-  if (size < 10) {
+function tree(size: number) {
+  if (size < 25) {
     return
   }
-  ctx.moveTo(0,0)
-  ctx.lineTo(0,-size)
-  ctx.stroke()
-  ctx.translate(0,-size)
-  ctx.save()
-  ctx.rotate(20*(Math.PI/180))
-  tree(size*0.8)
-  ctx.restore()
-  ctx.save()
-  ctx.rotate(-20*(Math.PI/180))
-  tree(size*0.8)
-  ctx.restore()
+  ctx.moveTo(0, 0);
+  ctx.lineTo(0, -size);
+  ctx.stroke();
+  ctx.translate(0, -size);
+  ctx.save();
+  ctx.rotate(30 * (Math.PI / 180));
+  tree(size * 0.8);
+  ctx.restore();
+  ctx.save();
+  ctx.rotate(-30 * (Math.PI / 180));
+  tree(size * 0.8);
+  ctx.restore();
+  ctx.save();
+  tree(size * 0.8);
+  ctx.restore();
 }
-ctx.translate(canvas.width/2,canvas.height)
+ctx.fillStyle = "blue"
+ctx.fillRect(0, 0, canvas.width, canvas.height)
+ctx.strokeStyle = "yellow"
+ctx.translate(canvas.width / 2, canvas.height)
 tree(100)
 
