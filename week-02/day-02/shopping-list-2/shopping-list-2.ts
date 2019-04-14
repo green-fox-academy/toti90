f123126f9560d18607049c3ace2aa2cf5306bcbd
tothtomi <tothtomi7@gmail.com>
2019-04-14 07:50:05 +0200

    feat(shopping_list_2): add the correct solution

--- week-02/day-02/shopping-list-2/shopping-list-2.ts
+++ week-02/day-02/shopping-list-2/shopping-list-2.ts
@@ -42,7 +42,7 @@ export{};
 // Who buys more products? (piece)
 
 
-let shopList1 = {
+let priceList:any = {
     "Milk":	1.07,
     "Rice":	1.59,
     "Eggs":	3.14,
@@ -54,7 +54,7 @@ let shopList1 = {
     "Onion":	1.10,
 }
 
-let shopList2 = {
+let bobList:any = {
     "Milk":	3,
     "Rice":	2,
     "Eggs":	2,
@@ -65,7 +65,7 @@ let shopList2 = {
     "Potato":	1
 }
 
-let shopList3 = {
+let aliceList: any = {
     "Rice":	1,
     "Eggs":	5,
     "Chicken Breasts":	2,
@@ -73,22 +73,54 @@ let shopList3 = {
     "Tomato":	10
 }
 
-function shopApp(obj1:any,obj2:any,obj3:any) {
-    let names = ["Tomi", "Bob", "Alice"]
-    let bobsum = 0;
-    let bobSum = Object.keys(obj1).map(x => bobsum+=obj1[x])
-    let tomisum = 0;
-    let tomiSum = Object.keys(obj1).map(x => tomisum+=obj1[x])
-    let alicesum = 0;
-    let aliceSum = Object.keys(obj3).map(x => alicesum+=obj3[x])
-    let ricesNumbers = [obj1["Rice"],obj2["Rice"],obj3["Rice"]]
-    let moreRiceBuyer = names[ricesNumbers.indexOf(Math.max(...ricesNumbers))]
-    let itemNumbers = [Object.keys(obj1).length,Object.keys(obj2).length,Object.keys(obj3).length]
-    let moreItemBuyers = names[itemNumbers.indexOf(Math.max(...itemNumbers))]
-    let amountNumbers: number[] = [tomiSum[tomiSum.length-1], bobSum[bobSum.length-1], aliceSum[aliceSum.length-1]];
-    let moreAmountBuyers = names[amountNumbers.indexOf(Math.max(...amountNumbers))]
-    return `Bob pay sum: ${bobSum[bobSum.length-1]}\nAlice pay sum: ${aliceSum[aliceSum.length-1]}
-${moreRiceBuyer} bought the most rice\n${moreItemBuyers} bought the most item\n${moreAmountBuyers} bough the most amount`;
+function shopApp(price:any,obj1:any,obj2:any) {
+    const bobKeys:string[] = Object.keys(obj1)
+    const aliceKeys:string[] = Object.keys(obj2)
+    let price_bob = 0
+    let item_bob = 0
+    let price_alice = 0
+    let item_alice = 0
+    for (let i = 0; i < bobKeys.length; i++) {
+        price_bob += obj1[bobKeys[i]] * price[bobKeys[i]]
+        item_bob += obj1[bobKeys[i]]
+    }
+    for (let i = 0; i < aliceKeys.length; i++) {
+        price_alice += obj2[aliceKeys[i]] * price[aliceKeys[i]]
+        item_alice += obj2[aliceKeys[i]]
+    }
+    let whoRice = (obj1.Rice > obj2.Rice ? "Bob":"Alice")
+    let whoPotato = (obj1.Potato > obj2.Potato ? "Bob":"Alice")
+    let whoDifferent = (bobKeys.length > aliceKeys.length ? "Bob":"Alice")
+    let whoItem = (item_bob > item_alice ? "Bob":"Alice")
+    return `How much does bob pay?: ${price_bob}
+    How much does alice pay?: ${price_alice}
+    Who buys more rice?: ${whoRice}
+    Who buys more potato?: ${whoPotato}
+    Who buys more different products?: ${whoDifferent}
+    Who buys more products? (piece): ${whoItem}`
 }
 
-console.log(shopApp(shopList1,shopList2,shopList3));
+// function shopApp2(obj1:any,obj2:any,obj3:any) {
+//     //reduce
+//     let names = ["Tomi", "Bob", "Alice"]
+//     let bobsum = 0;
+//     let bobSum = Object.keys(obj1).map(x => bobsum+=obj1[x])
+//     let tomisum = 0;
+//     let tomiSum = Object.keys(obj1).map(x => tomisum+=obj1[x])
+//     let alicesum = 0;
+//     let aliceSum = Object.keys(obj3).map(x => alicesum+=obj3[x])
+//     let ricesNumbers = [obj1["Rice"],obj2["Rice"],obj3["Rice"]]
+//     let moreRiceBuyer = names[ricesNumbers.indexOf(Math.max(...ricesNumbers))]
+//     let itemNumbers = [Object.keys(obj1).length,Object.keys(obj2).length,Object.keys(obj3).length]
+//     let moreItemBuyers = names[itemNumbers.indexOf(Math.max(...itemNumbers))]
+//     let amountNumbers: number[] = [tomiSum[tomiSum.length-1], bobSum[bobSum.length-1], aliceSum[aliceSum.length-1]];
+//     let moreAmountBuyers = names[amountNumbers.indexOf(Math.max(...amountNumbers))]
+//     return `Bob pay sum: ${bobSum[bobSum.length-1]}\nAlice pay sum: ${aliceSum[aliceSum.length-1]}
+// ${moreRiceBuyer} bought the most rice\n${moreItemBuyers} bought the most item\n${moreAmountBuyers} bough the most amount`;
+// }
+
+
+console.log(shopApp(priceList,bobList,aliceList))
+
+
+
