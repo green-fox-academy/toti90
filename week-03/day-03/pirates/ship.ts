c341f7c04259b7169ead124abd51c41ab713f590
tothtomi <tothtomi7@gmail.com>
2019-04-18 13:01:06 +0200

    feat(pirates): add ships, and battle

---
+++ week-03/day-03/pirates/ship.ts
@@ -0,0 +1,78 @@
+import { Pirate } from "./pirate";
+
+`use strict`
+export{}
+
+export class Ship {
+  crew: Pirate[] = [];
+  numberOfLivePireate:number = 0;
+
+  // constructor(captain?:Pirate) {
+  //   this.captain = captain
+  // }
+
+
+  fillShip() {
+    let number = Math.floor(Math.random()*10+1)
+    let pirates = []
+
+    for (let i=0;i<number;i++) {
+      let pirate = new Pirate()
+      pirates.push(pirate)
+      if (i === 0) {
+        pirate.isCaptain = true 
+      } 
+    }
+    this.crew = pirates
+    this.numberOfLivePireate = this.crew.length
+  }
+
+  print() {
+    console.log(`Captain consumed ${this.crew[0].drunk} rum(s), and his state is: ${this.crew[0].howItGoingMate()}`)
+    console.log(`Number of alive pirates is: ${this.crew.length}`)
+  }
+
+  battle(otherShip: Ship) {
+    let win = true;
+    let score1 = this.crew.length - this.crew[0].drunk
+    let score2 = otherShip.crew.length - otherShip.crew[0].drunk
+    let winnerShip:Pirate[]
+    let looserShip: Pirate[]
+    if (score1 > score2) {
+      winnerShip = this.crew
+      looserShip = otherShip.crew
+    } else {
+      winnerShip = otherShip.crew
+      looserShip = this.crew
+      win=false
+    }
+    
+    let numberOfDeath = Math.floor(Math.random()*looserShip.length)
+    for (let i=0;i<numberOfDeath;i++) {
+      looserShip[i].die()
+      looserShip.splice(i,1)
+    }
+    let numberOfRums = Math.floor(Math.random()*10)
+    while (numberOfRums !== 0) {
+      let randomPersonIndex = Math.floor(Math.random()*(winnerShip.length+1))
+      winnerShip[randomPersonIndex].drinkSomeRum()
+      numberOfRums--
+    }
+    return win
+  }
+
+}
+
+let ship1 = new Ship()
+let ship2 = new Ship()
+console.log(ship1.crew)
+console.log(ship2.crew)
+ship1.fillShip()
+ship2.fillShip()
+console.log(ship1.crew)
+console.log(ship2.crew)
+console.log(ship1.battle(ship2))
+console.log(ship1.crew)
+console.log(ship2.crew)
+
+
