449340a681985909f329d88354177e5ff709ab2b
tothtomi <tothtomi7@gmail.com>
2019-04-23 11:48:48 +0200

    feat(greenfox_organization): add solution

---
+++ week-04/day-02/greenfox_organization/cohort.ts
@@ -0,0 +1,28 @@
+import { Student } from "./student";
+import { Mentor } from "./mentor";
+
+`use strict`
+
+export class Cohort {
+  name: string;
+  students: Student[];
+  mentors: Mentor[];
+
+  constructor(name: string) {
+    this.name = name;
+    this.students = [];
+    this.mentors = [];
+  }
+
+  addStudent(student: Student) {
+    this.students.push(student);
+  }
+
+  addMentor(mentor: Mentor) {
+    this.mentors.push(mentor);
+  }
+
+  info() {
+    console.log(`The ${this.name} cohort has ${this.students.length} students and ${this.mentors.length} mentors.`);
+  }
+}
---
+++ week-04/day-02/greenfox_organization/mentor.ts
@@ -0,0 +1,21 @@
+import { Person } from "./person";
+
+`use strict`
+
+export class Mentor extends Person {
+  level: string;
+
+  constructor(name: string = "Jane Doe", age: number = 30, gender: string = "female", level: string = "intermediate") {
+    super(name, age, gender);
+    this.level = level;
+  }
+
+  getGoal() {
+    console.log('My goal is: Educate brilliant junior software developers.');
+  }
+
+  introduce() {
+    console.log(`Hi, I'm ${this.name}, a ${this.age} year old ${this.gender} ${this.level} mentor.`);
+  }
+}
+
