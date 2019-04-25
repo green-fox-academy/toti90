ea8b6d872fdf6c1d005e86849bd66595ea7f8275
tothtomi <tothtomi7@gmail.com>
2019-04-25 15:54:33 +0200

    feat(cowsAndBulls): put guessing game part to main.ts

--- week-04/day-04/cowsAndBulls/main.ts
+++ week-04/day-04/cowsAndBulls/main.ts
@@ -2,14 +2,16 @@
 
 
 import {CowsAndBulls} from './cowsAndBulls'
-let game = new CowsAndBulls(3462)
+let game = new CowsAndBulls(3042)
+console.log(game.answer)
 const inputReader = require('readline-sync');
 let userNumber:number
 while (game.isPlaying) {
-  userNumber = inputReader.question("Please guess a four number digit: ");
-  console.log(typeof userNumber)
-  console.log(`Round ${game.numberOfGuesses}\nYour number: ${userNumber}\nYour result is: ${game.guess(userNumber)}`);
+  userNumber = inputReader.question("Please guess a four different number digit: ");
+  console.log(`-----------`)
+  console.log(`Round ${game.numberOfGuesses}. You have got ${8-game.numberOfGuesses} round left\nYour number: ${userNumber}\nYour result is: ${game.guess(userNumber)}`);
 }
 if (game.numberOfGuesses === 9) {
   console.log("You lost, you havent got more round")
 }
+
