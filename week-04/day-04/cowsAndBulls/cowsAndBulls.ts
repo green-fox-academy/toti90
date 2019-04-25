545efc375a0a0eed0387c46f08c6201504cc9384
tothtomi <tothtomi7@gmail.com>
2019-04-25 14:55:34 +0200

    feat(cowsAndBulls): user can guess, for 8 round

--- week-04/day-04/cowsAndBulls/cowsAndBulls.ts
+++ week-04/day-04/cowsAndBulls/cowsAndBulls.ts
@@ -5,10 +5,10 @@ export class CowsAndBulls {
   public isPlaying: boolean;
   public numberOfGuesses: number;
 
-  constructor(pAnswer: number) {
+  constructor(pAnswer: number = Math.floor(1000 + Math.random() * 9000)) {
     this.answer = pAnswer;
     this.isPlaying = true;
-    this.numberOfGuesses = 0;
+    this.numberOfGuesses = 1;
   }
 
   guess(playerGuess: number): string {
@@ -29,11 +29,25 @@ export class CowsAndBulls {
         bulls++
       }
     }
-    if (this.numberOfGuesses === 7) {
+    if (this.numberOfGuesses === 8) {
       this.isPlaying = false
     }
     this.numberOfGuesses++
+    if (cows === 4) {
+      this.isPlaying = false;
+      return `YEAAAH YOU WIN`
+    }
     return `${cows} cow,${bulls} bull`
   }
 
+}
+let game = new CowsAndBulls(3462)
+const inputReader = require('readline-sync');
+let userNumber:number
+while (game.isPlaying) {
+  userNumber = inputReader.question("Please guess a four number digit: ");
+  console.log(`Round ${game.numberOfGuesses}\nYour number: ${userNumber}\nYour result is: ${game.guess(userNumber)}`);
+}
+if (game.numberOfGuesses === 9) {
+  console.log("You lost, you havent got more round")
 }
