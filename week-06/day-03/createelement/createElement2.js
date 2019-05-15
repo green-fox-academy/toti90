fa2edf95a7596d4bb880f7f7fd809e5ea646ff12
tothtomi <tothtomi7@gmail.com>
2019-05-15 11:44:05 +0200

    add solution createElement2

---
+++ week-06/day-03/createelement/createElement2.js
@@ -0,0 +1,46 @@
+// Remove the king from the list.
+
+// Fill the list based on the following list of objects.
+// Only add the asteroids to the list.
+// Each list item should have its category as a class and its content as text content.
+
+const planetData = [
+  {
+  category: 'inhabited',
+  content: 'Foxes',
+  asteroid: true,
+  },
+  {
+  category: 'inhabited',
+  content: 'Whales and Rabbits',
+  asteroid: true,
+  },
+  {
+  category: 'uninhabited',
+  content: 'Baobabs and Roses',
+  asteroid: true,
+  },
+  {
+  category: 'inhabited',
+  content: 'Giant monsters',
+  asteroid: false,
+  },
+  {
+  category: 'inhabited',
+  content: 'Sheep',
+  asteroid: true,
+  },
+  ];
+  var liKing = document.querySelector('li')
+  var ul = document.querySelector('ul')
+  ul.removeChild(liKing)
+  for (let i=0; i<planetData.length;i++) {
+    if (planetData[i].asteroid === true) {
+      var li = document.createElement('li')
+      li.setAttribute('class', planetData[i].category)
+      li.innerHTML = planetData[i].content
+      console.log(li.className)
+      ul.appendChild(li)
+    }
+  }
+
