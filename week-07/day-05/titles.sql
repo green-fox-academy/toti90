f233173724d32da2600089478c2197dd9b959328
tothtomi <tothtomi7@gmail.com>
2019-05-24 16:12:13 +0200

    generate tables

--- week-07/day-05/titles.sql
+++ week-07/day-05/titles.sql
@@ -1,3 +1,5 @@
+DROP DATABASE reddit;
+CREATE DATABASE reddit;
 USE reddit;
 
 CREATE TABLE `posts` (
@@ -10,10 +12,11 @@ CREATE TABLE `posts` (
 	KEY `id` (`id`) USING BTREE
 );
 
-INSERT INTO posts(title, url, owner_name) VALUES ('Hello World', 'http://helloworld.com', 'pistike');
-INSERT INTO posts(title, url, owner_name) VALUES ('Cat world', 'http://cats.com', 'pistike');
-INSERT INTO posts(title, url, owner_name) VALUES ('Table Tennis Daily', 'http://pingpong.hu', 'pistike');
-INSERT INTO posts(title, url, owner_name) VALUES ('Green Fox Academy', 'http://zoldroka.hu', 'pistike');
+INSERT INTO posts(title, url, owner_name) VALUES ('Hello World', 'http://helloworld.com', 'Feri');
+INSERT INTO posts(title, url, owner_name) VALUES ('Cat world', 'http://cats.com', 'Marcsi');
+INSERT INTO posts(title, url, owner_name) VALUES ('Table Tennis Daily', 'http://pingpong.hu', 'Feri');
+INSERT INTO posts(title, url, owner_name) VALUES ('Green Fox Academy', 'http://zoldroka.hu', 'Pist');
+INSERT INTO posts(title, url, owner_name) VALUES ('Something', 'http://something.hu', 'Pisti');
 
 CREATE TABLE `users` (
 	`user_id` INT NOT NULL AUTO_INCREMENT,
@@ -24,6 +27,20 @@ CREATE TABLE `users` (
 INSERT INTO users(user_name) VALUES ('Feri');
 INSERT INTO users(user_name) VALUES ('Pisti');
 INSERT INTO users(user_name) VALUES ('Marcsi');
+
+CREATE TABLE `votes` (
+	`post_id` INT NOT NULL,
+	`user_name` CHAR(100) NOT NULL,
+  `vote` INT
+ );
+
+ INSERT INTO votes(post_id, user_name, vote) VALUES (1,'Feri',1);
+ INSERT INTO votes(post_id, user_name, vote) VALUES (2,'Feri',1);
+ INSERT INTO votes(post_id, user_name, vote) VALUES (3,'Feri',1);
+ INSERT INTO votes(post_id, user_name, vote) VALUES (5,'Pisti',-1);
+ INSERT INTO votes(post_id, user_name, vote) VALUES (1,'Marcsi',1);
+ INSERT INTO votes(post_id, user_name, vote) VALUES (2,'Marcsi',1);
+ INSERT INTO votes(post_id, user_name, vote) VALUES (4,'Marcsi',1);
 
 
 
