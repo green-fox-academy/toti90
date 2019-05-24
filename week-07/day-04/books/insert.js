a32ba1fc5b5934810a22b61f38ebfd4076e46725
tothtomi <tothtomi7@gmail.com>
2019-05-24 09:04:45 +0200

    insert all rows from csv

--- week-07/day-04/books/insert.js
+++ week-07/day-04/books/insert.js
@@ -11,8 +11,8 @@ const conn = mysql.createConnection({
   insecureAuth: true,
   database: 'bookstore',
 });
-let table = 'users'
-let cols = ['id', 'prefix', 'first_name', 'last_name', 'addresse', 'height', 'bitcoin_adresse', 'color_preference']
+
+
 
 conn.connect(err => {
   if (err) {
@@ -22,9 +22,8 @@ conn.connect(err => {
   console.log('connection to DB is OK ✨');
 });
 
-
 let query1 = `USE bookstore;`
-conn.query(query1,function (err, rows) {
+conn.query(query1, function (err, rows) {
   if (err) {
     console.log(err.toString());
     return;
@@ -34,48 +33,55 @@ conn.query(query1,function (err, rows) {
   }
 });
 
-let query2 = `
-CREATE TABLE IF NOT EXISTS ${table} (
-	${cols[0]} INT NOT NULL AUTO_INCREMENT,
-	${cols[1]} VARCHAR(20),
-	${cols[2]} VARCHAR(30) NOT NULL,
-	${cols[3]} VARCHAR(30) NOT NULL,
-	${cols[4]} VARCHAR(100),
-	${cols[5]} SMALLINT,
-	${cols[6]} VARCHAR(255),
-	${cols[7]} TEXT(10),
-	KEY Id (id) USING BTREE
-);`
-conn.query(query2,function (err, rows) {
-  if (err) {
-    console.log(err.toString());
-    return;
-  } else {
-    console.log('Make table')
-    return;
-  }
-});
-
-
 try {
   let content = fs.readFileSync('./books/users.csv', "UTF8");
   let rows = content.split('\n')
-  for (let i=0;i<rows.length;i++) {
+  let insertion;
+  let cols;
+  let table = 'users'
+  for (let i = 0; i < rows.length; i++) {
     let words = rows[i].split(",")
-    let insertion = {'id': parseInt(words[0]), 
-    'prefix': words[1], 
-    'first_name': words[2], 
-    'last_name': words[3], 
-    'addresse': words[4], 
-    'height': parseFloat(words[5]), 
-    'bitcoin_adresse': words[6], 
-    'color_preference':words[7]}
-    conn.query('INSERT INTO users SET ?', insertion,function (err, rows) {
-      if (err) {
-        console.log(err.toString());
-        return;
-      } 
-    });
+    if (i === 0) {
+      cols = words
+      let query2 = `
+        CREATE TABLE IF NOT EXISTS ${table} (
+          ${cols[0]} INT NOT NULL AUTO_INCREMENT,
+          ${cols[1]} VARCHAR(20),
+          ${cols[2]} VARCHAR(30) NOT NULL,
+          ${cols[3]} VARCHAR(30) NOT NULL,
+          ${cols[4]} VARCHAR(100),
+          ${cols[5]} VARCHAR(100),
+          ${cols[6]} VARCHAR(255),
+          ${cols[7]} TEXT(10),
+          KEY Id (id) USING BTREE
+        );`
+        conn.query(query2, function (err, rows) {
+          if (err) {
+            console.log(err.toString());
+            return;
+          } else {
+            console.log('Make table')
+            return;
+          }
+        });
+    } else {
+      insertion = {
+        'id': parseInt(words[0]),
+        'prefix': words[1],
+        'first_name': words[2],
+        'last_name': words[3],
+        'address': words[4],
+        'height': words[5],
+        'bitcoin_address': words[6],
+        'color_preference': words[7]
+      }
+      conn.query('INSERT INTO users SET ?', insertion, function (err, rows) {
+        if (err) {
+          console.log(err.toString());
+          return;
+        }
+      });
+    }
   }
   console.log('Added datas to table')
 }
