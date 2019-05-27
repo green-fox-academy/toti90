b94fb2cd63489f243636b07c5ee88f1dd8bdc8e3
tothtomi <tothtomi7@gmail.com>
2019-05-27 16:27:05 +0200

    tennis game solution

---
+++ week-08/day-01/TennisGame1.js
@@ -0,0 +1,48 @@
+class TennisGame1 {
+  playerOneScore = 0;
+  playerTwoScore = 0;
+
+  constructor(player1Name, player2Name) {
+    this.player1Name = player1Name;
+    this.player2Name = player2Name;
+  }
+
+  wonPoint(playerName) {
+    (playerName === "player1" ? this.playerOneScore++ : this.playerTwoScore++)
+  }
+
+  getScore() {
+    if (this.playerOneScore === this.playerTwoScore) {
+      if (this.playerOneScore<3) {
+        return `${getScore(this.playerOneScore)}-All`
+      } else {
+        return 'Deuce'
+      }
+    } else if (this.playerOneScore >= 4 || this.playerTwoScore >= 4) {
+      var minusResult = this.playerOneScore - this.playerTwoScore;
+      if (minusResult === 1) return "Advantage player1";
+      else if (minusResult === -1) return "Advantage player2";
+      else if (minusResult >= 2) return "Win for player1";
+      else return "Win for player2";
+    } else {
+      return `${getScore(this.playerOneScore)}-${getScore(this.playerTwoScore)}`
+    }
+  };
+}
+
+function getScore(score) {
+  switch (score) {
+    case 0:
+      return "Love";
+    case 1:
+      return "Fifteen";
+    case 2:
+      return "Thirty";
+    case 3:
+      return "Forty";
+  }
+}
+
+if (typeof window === "undefined") {
+  module.exports = TennisGame1;
+}
---
+++ week-08/day-01/TennisGame3.js
@@ -0,0 +1,33 @@
+var TennisGame3 = function(p1Name, p2Name) {
+  this.p2Score = 0;
+  this.p1Score = 0;
+
+  this.p1Name = p1Name;
+  this.p2Name = p2Name;
+};
+
+TennisGame3.prototype.getScore = function() {
+  var score;
+  if ((this.p1Score < 4 && this.p2Score < 4) && (this.p1Score + this.p2Score < 6)) {
+      var p = ["Love", "Fifteen", "Thirty", "Forty"];
+      score = p[this.p1Score];
+      return (this.p1Score == this.p2Score) ? score + "-All" : score + "-" + p[this.p2Score];
+  } else {
+      if (this.p1Score == this.p2Score)
+          return "Deuce";
+      score = this.p1Score > this.p2Score ? this.p1Name : this.p2Name;
+      return (Math.abs(this.p1Score - this.p2Score) == 1) ? "Advantage " + score : "Win for " + score;
+  }
+};
+
+TennisGame3.prototype.wonPoint = function(playerName) {
+  if (playerName == "player1")
+      this.p1Score += 1;
+  else
+      this.p2Score += 1;
+
+};
+
+if (typeof window === "undefined") {
+  module.exports = TennisGame3;
+}
