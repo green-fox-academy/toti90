b5633a27d1f571daf0bdd8d1e22861e10feb213f
tothtomi <tothtomi7@gmail.com>
2019-06-20 17:10:56 +0200

    test modify

--- pp/weather-app/src/app/weather-api.service.spec.ts
+++ pp/weather-app/src/app/weather-api.service.spec.ts
@@ -1,16 +1,34 @@
 import { TestBed } from '@angular/core/testing';
-import { HttpClientTestingModule} from '@angular/common/http/testing'
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing'
 import { WeatherApiService } from './weather-api.service';
 
 fdescribe('WeatherApiService', () => {
 
-let service: WeatherApiService
+  let service: WeatherApiService
+  let httpMock: HttpTestingController
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-    imports: [HttpClientTestingModule],
-    providers: [WeatherApiService]
-  });
-  service = TestBed.get(WeatherApiService)
+      imports: [HttpClientTestingModule],
+      providers: [WeatherApiService]
+    });
+    service = TestBed.get(WeatherApiService)
+    httpMock = TestBed.get(HttpTestingController);
   })
+
+  it('should be equal', () => {
+    service.currentCity.subscribe((city) => {
+      // console.log(city.name)
+    })
+    const req = httpMock.expectOne('https://api.openweathermap.org/data/2.5/weather?q=Budapest&APPID=100cb1887e2abc22cbc5d54fc670e448')
+    // expect(req.request.method).toBe('GET');
+    // req.flush({
+    //   name: 'Budapest'
+    // });
+
+    // httpMock.verify();
+  })
+
+
+
 });
